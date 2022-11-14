219d1182961c0693be73196f557b09ef7632c53b
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-14 23:11:27 +0000

    Adding tarfile member sanitization to extractall()

--- fetch_litecore_base.py
+++ fetch_litecore_base.py
@@ -239,7 +239,26 @@ def download_variant(download_folder: str, variant: str, build: str, debug: bool
     conditional_print(f"--- Extracting {filename}...")
     if filename.endswith("tar.gz"):
         with tarfile.open(full_path, "r:gz") as tar:
-            tar.extractall(download_path)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, download_path)
     else:
         with zipfile.ZipFile(full_path) as zip:
             zip.extractall(download_path)
